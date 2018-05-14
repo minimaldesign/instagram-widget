3ca9d6bf871fb2b8951c8e6658756a30bac120a8
Yann <yann@minimaldesign.net>
2018-05-13 21:18:16 -0400

    add constants and option to set image as a background

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 Easily add a "latest photos" type slideshow to your website.
 
-You'll need to get an authemtication token from your instagram account in order to do that.
+You'll need to get an authemtication token from your instagram account in order to do that first though:
+
+1. Log in instagram andf [register as a developer][dev]
+1. Register a new "client" (Sandbox Mode is good enough)
+1. Copy your "Client ID"
+1. Make sure you setup the "Valid redirect URIs" under the Security tab
+1. Authenticate you client by going to https://api.instagram.com/oauth/authorize/?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=token
+1. Once you authenticate, you'll be redirected to your redirect URL and the token will be appended to it
+1. Add your token to the `AUTH_TOKEN` constant
+
+
+[dev]:https://www.instagram.com/developer/
--- instagram.js
+++ instagram.js
@@ -1,64 +1,87 @@
+(function init() {
+  // see README.md for how to setup your token
+  const AUTH_TOKEN = '1518392443.7db5355.e9cbaf65eeb3499eb3544e834aecf8dd';
+  // element the slideshow will be added to. Styles in instagram.css
+  const TARGET_SELECTOR = '.instagram-widget';
+  // how long the fade out between slides takes to kick in (fade out speed itself in CSS)
+  const SPEED = 3000;
+  // by default, images are added as <img /> but if you don't always post square images on Instagram
+  // you might want change this to `true` to set them up as <li /> background images instead
+  const BG_IMG = false;
 
-function preloadImages(urls) {
-  function loadImage(url) {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => {
-        resolve(img);
-      };
-      img.onerror = () => {
-        reject(img); // TODO: log image that doesn't load
-      };
-      img.src = url;
+
+  function preloadImages(urls) {
+    function loadImage(url) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+          resolve(img);
+        };
+        img.onerror = () => {
+          reject(img); // TODO: log image that doesn't load
+        };
+        img.src = url;
+      });
+    }
+    const promises = [];
+    urls.forEach((url) => {
+      promises.push(loadImage(url));
+    });
+    return Promise.all(promises);
+  }
+
+  function listImages(images) {
+    const target = document.querySelector(TARGET_SELECTOR);
+    const list = document.createElement('ul');
+    images.forEach((image) => {
+      const item = document.createElement('li');
+      item.appendChild(image);
+      list.appendChild(item);
+    });
+    target.appendChild(list);
+  }
+
+  function listImagesAsBackground(urls) {
+    const target = document.querySelector(TARGET_SELECTOR);
+    const list = document.createElement('ul');
+    list.innerHTML = urls.map(url => `<li style="background-image: url(${url})"></li>`).join('');
+    target.appendChild(list);
+  }
+
+  function fadeImages() {
+    const images = [...document.querySelectorAll(`${TARGET_SELECTOR} li`)];
+    const totalImages = images.length;
+    let currentImage = totalImages - 1;
+    const fade = () => {
+      setTimeout(() => {
+        images[currentImage].classList.add('fade');
+        if (currentImage === 0) {
+          images.forEach(img => img.classList.remove('fade'));
+          currentImage = totalImages - 1;
+        } else {
+          currentImage -= 1;
+        }
+        fade();
+      }, SPEED);
+    };
+    fade();
+  }
+
+  async function getInsta() {
+    const url = `https://api.instagram.com/v1/users/self/media/recent?count=10&access_token=${AUTH_TOKEN}`;
+    const imagesUrls = await fetch(url)
+      .then(data => data.json())
+      .then(data => data.data.reverse().map(item => item.images.standard_resolution.url));
+
+    preloadImages(imagesUrls).then((images) => {
+      if (BG_IMG) {
+        listImagesAsBackground(imagesUrls);
+      } else {
+        listImages(images);
+      }
+      fadeImages();
     });
   }
-  const promises = [];
-  urls.forEach((url) => {
-    promises.push(loadImage(url));
-  });
-  return Promise.all(promises);
-}
 
-function listImages(images) {
-  const target = document.querySelector('.instagram-widget');
-  const list = document.createElement('ul');
-  images.forEach((image) => {
-    const item = document.createElement('li');
-    item.appendChild(image);
-    list.appendChild(item);
-  });
-  target.appendChild(list);
-}
-
-function fadeImages() {
-  const images = [...document.querySelectorAll('.instagram-widget li')];
-  const totalImages = images.length;
-  let currentImage = totalImages - 1;
-  const fade = () => {
-    setTimeout(() => {
-      images[currentImage].classList.add('fade');
-      if (currentImage === 0) {
-        images.forEach(img => img.classList.remove('fade'));
-        currentImage = totalImages - 1;
-      } else {
-        currentImage -= 1;
-      }
-      fade();
-    }, 3000);
-  };
-  fade();
-}
-
-async function getInsta() {
-  const token = 'TOKEN_GOES_HERE';
-  const url = `https://api.instagram.com/v1/users/self/media/recent?count=10&access_token=${token}`;
-  const imagesUrls = await fetch(url)
-    .then(data => data.json())
-    .then(data => data.data.reverse().map(item => item.images.standard_resolution.url));
-  preloadImages(imagesUrls).then((images) => {
-    listImages(images);
-    fadeImages();
-  });
-}
-
-getInsta();
+  getInsta();
+}());
